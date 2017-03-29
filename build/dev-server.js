cc56305a840dceab0cba08b041e10b0a7ae30799
jiaolong1021 <2278371826@qq.com>
2017-03-29 19:53:19 +0800

    remove data.json

--- build/dev-server.js
+++ build/dev-server.js
@@ -28,36 +28,6 @@ var proxyTable = config.dev.proxyTable
 var app = express()
 var compiler = webpack(webpackConfig)
 
-/* 添加内容 */
-var appData = require("../data.json");
-var seller = appData.seller;
-var goods = appData.goods;
-var ratings = appData.ratings;
-var apiRoutes = express.Router();
-apiRoutes.get("/seller", function (req, res) {
-  res.json({
-    errno: 0,
-    data: seller
-  });
-});
-
-apiRoutes.get("/goods", function (req, res) {
-  res.json({
-    errno: 0,
-    data: goods
-  });
-});
-
-apiRoutes.get("/ratings", function (req, res) {
-  res.json({
-    errno: 0,
-    data: ratings
-  });
-});
-app.use('/api', apiRoutes);
-
-/* /添加内容 */
-
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
   quiet: true
